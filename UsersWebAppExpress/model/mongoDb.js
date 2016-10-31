005e14ee6c36cba32199fe42e6ce39cb6948b6aa
Mateusz Waszkiewicz <m.waszkiewicz88@o2.p>
2016-10-31 18:30:33 +0100

    test

--- UsersWebAppExpress/model/mongoDb.js
+++ UsersWebAppExpress/model/mongoDb.js
@@ -3,10 +3,11 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/usersDb');
 var mongoSchema = mongoose.Schema;
-var userSchema  = {
-    "userName" : String,
-    "fullName" : String,
-    "email" : String
+var userSchema = {
+    "userName": String,
+    "fullName": String,
+    "email": String
 };
 
-module.exports = mongoose.model('users',userSchema);
+
+module.exports = mongoose.model('users', userSchema);
